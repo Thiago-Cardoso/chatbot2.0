492cfac3110742f7cf9925a3abc89c5f28b2c30a
Thiago-Cardoso <trovador182@gmail.com>
2019-03-09 11:22:58 -0300

    creation of models

--- Rakefile
+++ Rakefile
@@ -1,4 +1,5 @@
 require 'sinatra/activerecord'
 require 'sinatra/activerecord/rake'
+require 'pg_search'
 load "pg_search/tasks.rb"
 require './app'
--- app.rb
+++ app.rb
@@ -3,6 +3,9 @@ require 'sinatra'
 require 'sinatra/activerecord'
   
 require './config/database'
+
+# Load Models
+Dir["./app/models/*.rb"].each {|file| require file }
   
 class App < Sinatra::Base
   get '/' do
