e80e782ece25d313dbb5c2c0148f67f04bdc6d73
Thiago-Cardoso <trovador182@gmail.com>
2019-03-09 13:20:16 -0300

    added file for webhook

--- app.rb
+++ app.rb
@@ -1,14 +1,36 @@
 require 'json'
 require 'sinatra'
 require 'sinatra/activerecord'
-  
+ 
 require './config/database'
-
-# Load Models
+ 
 Dir["./app/models/*.rb"].each {|file| require file }
-  
+Dir["./app/services/**/*.rb"].each {|file| require file }
+ 
 class App < Sinatra::Base
+ 
   get '/' do
-    'Hello world!'
+    'Hello world!'   
+  end
+ 
+  post '/webhook' do
+    request.body.rewind
+    result = JSON.parse(request.body.read)["queryResult"]
+ 
+    if result["contexts"].present?
+      response = InterpretService.call(result["action"], result["contexts"][0]["parameters"])
+    else
+      response = InterpretService.call(result["action"], result["parameters"])
+    end
+ 
+    content_type :json, charset: 'utf-8'
+    {
+      "payload": {
+        "telegram": {
+          "text": response,
+          "parse_mode": "Markdown"
+        }
+      }
+    }.to_json
   end
 end
